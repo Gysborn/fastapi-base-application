923c5599135d69164ae0a4f635598da52128d28b
Gysborn <forparty@gmail.com>
2025-08-11 13:21:57 +0300

    Change User class

--- src/core/models/base.py
+++ src/core/models/base.py
@@ -1,16 +1,14 @@
-from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
-
-from core.config import settings
 
 
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    metadata = MetaData(naming_convention=settings.db.naming_convention)
-
     @declared_attr.directive
     def __tablename__(cls) -> str:
+        # Лучше добавить проверку на абстрактные классы
+        if cls.__dict__.get("__abstract__", False):
+            return None
         return cls.__name__.lower()
 
     id: Mapped[int] = mapped_column(primary_key=True)
--- src/core/models/user.py
+++ src/core/models/user.py
@@ -4,4 +4,7 @@ from core.models import Base
 
 
 class User(Base):
+    __tablename__ = "users"
+    # __table_args__ = {"extend_existing": True}
+
     username: Mapped[str] = mapped_column(unique=True)
